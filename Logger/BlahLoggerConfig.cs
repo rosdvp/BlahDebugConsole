ee53503e87bcd3778d6784d52be1c56616b72def
rosdvp <45929573+rosdvp@users.noreply.github.com>
2023-10-18 15:39:19 +0300

    BlahLoggerConfig not serializable

--- Logger/BlahLoggerConfig.cs
+++ Logger/BlahLoggerConfig.cs
@@ -1,15 +1,10 @@
-﻿using System;
-
-namespace BlahDebugConsole.Logger
+﻿namespace BlahDebugConsole.Logger
 {
-[Serializable]
 public class BlahLoggerConfig
 {
 	public ELogType MinimalLogType;
 	public bool     IsWriteIntoFile;
 	public float    WriteIntoFileInterval;
-
-	[NonSerialized]
-	public bool UseCustomListener;
+	public bool     UseCustomListener;
 }
 }
